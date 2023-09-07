711f934203ee96a09407c7dee1b51d2463a19d6b
Wael Karman <wkarman@data-modul.com>
2023-09-07 14:34:42 +0200

    fix

--- publisher4PWM.py
+++ publisher4PWM.py
@@ -14,27 +14,31 @@ import zmq
 
 def main(url=None):
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    print(s.getsockname()[0])
+    
     ctx = zmq.Context.instance()
     publisher = ctx.socket(zmq.PUB)
     publisher.bind("tcp://*:5557")
-    # Ensure subscriber connection has time to complete
-    time.sleep(1)
 
     while True:
         # Send one random update per second
         try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(("8.8.8.8", 80))
+            print(s.getsockname()[0])
+
+            # Ensure subscriber connection has time to complete
             time.sleep(1)
             ipstr = f"AWAKE ON {s.getsockname()[0]}"
             publisher.send_multipart([
                 b"PASSIVEBUZZER",
                 bytes(ipstr, 'utf-8'),
             ])
-        except KeyboardInterrupt:
-            print("interrupted")
-            break
+        except Exception as e:
+            publisher.send_multipart([
+                b"PASSIVEBUZZER",
+                bytes("NOT CONNECTED", 'utf-8'),
+            ])
+            
 
 if __name__ == '__main__':
     main(sys.argv[1] if len(sys.argv) > 1 else None)
